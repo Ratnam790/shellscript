df1b03affe441f3e4cd06d05232325d14c45f08f
Ratnam790 <ratnammunganda1@gmail.com>
2024-06-20 15:38:07 +0530

    first code

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,10 +2,10 @@
 
 echo "plear enter username:"
 
-read USERNAME
+read -s USERNAME
 
 echo "please enter password:"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "username is: $USERNAME, password is: $PASSWORD"
