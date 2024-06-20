ea4c4a3a33eb13e529d62a6474ea162b01b062e7
Ratnam790 <ratnammunganda1@gmail.com>
2024-06-20 15:12:32 +0530

    first code

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: hi $PERSON2 how are you"
+echo "$PERSON2 :: hi $PERSON1 im fine what about you"
+echo "$PERSON1 :: how about your family"
+echo "$PERSON2 :: good my dear friend"
