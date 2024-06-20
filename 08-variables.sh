8b4287c633911d101490be86d8a16ea1c1a5ecde
Ratnam790 <ratnammunganda1@gmail.com>
2024-06-20 17:22:01 +0530

    commit message

--- 08-variables.sh
+++ 08-variables.sh
@@ -5,4 +5,8 @@ echo "NUMBER OF VARIABLES PASSED: $#"
 echo "script name: $0"
 echo "current working directory: $PWD"
 echo "home directory of current user: $HOME"
-
+echo "which user is running the script: $USER"
+echo "hostname: $HOSTNAME"
+echo "process id of the current shell script: $$"
+sleep 60 &
+echo "process id of last background command: $!"
