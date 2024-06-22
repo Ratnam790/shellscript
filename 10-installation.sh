c613a1317a5c74eacc76971dcd79f1b3df7c3915
Ratnam790 <ratnammunganda1@gmail.com>
2024-06-22 12:40:51 +0530

    commit message for mysql install

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,8 +6,29 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access."
+    exit 1 #manually exit if error comes.
 else
     echo "you are the super user"
 fi
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MYSQL is FAILURE"
+    exit 1
+else
+    echo "Installation of MYSQL is SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git is FAILURE"
+    exit 1
+else
+    echo "Installation of git is SUCCESS"
+fi
+
+echo "is script is proceeding"
 
